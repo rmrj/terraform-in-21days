386692020bff12f1009778a550b9dd9781c0e54f
Rama Raju konuganti <rama.konuganti@aveshasystems.com>
2023-05-04 16:01:20 -0700

    updated rebase - used count and global local variables

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,13 @@
 locals {
+<<<<<<< HEAD
   public_cidr        = ["10.0.0.0/24", "10.0.1.0/24"]
   private_cidr       = ["10.0.100.0/24", "10.0.101.0/24"]
   availability_zones = ["us-west-1a", "us-west-1b"]
+=======
+  public_cidr        = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  private_cidr       = ["10.0.100.0/24", "10.0.101.0/24", "10.0.103.0/24"]
+  availability_zones = ["us-west-1a", "us-west-1b", "us-west-1c"]
+>>>>>>> da2defa (used count and global local variables)
 }
 
 resource "aws_vpc" "main" {
