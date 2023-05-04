20ca78856f2147f8b463d05bd24071fc657cc695
Rama Raju konuganti <rama.konuganti@aveshasystems.com>
2023-05-04 16:29:30 -0700

    used variables to define envcode and public/private cidrs

--- vpc.tf
+++ vpc.tf
@@ -1,38 +1,36 @@
 locals {
-  public_cidr        = ["10.0.0.0/24", "10.0.1.0/24"]
-  private_cidr       = ["10.0.100.0/24", "10.0.101.0/24"]
   availability_zones = ["us-west-1a", "us-west-1b"]
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "main"
+    Name = var.env_code
   }
 }
 
 resource "aws_subnet" "public" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.public_cidr[count.index]
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "public${count.index + 1}"
+    Name = "${var.env_code}-public${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
 
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.private_cidr[count.index]
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "private${count.index + 1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
@@ -41,7 +39,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = var.env_code
   }
 }
 
@@ -54,35 +52,35 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "main"
+    Name = var.env_code
   }
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   vpc = true
 
   tags = {
-    Name = "nat${count.index + 1}"
+    Name = "${var.env_code}-nat${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "main${count.index + 1}"
+    Name = "${var.env_code}-main${count.index + 1}"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
@@ -91,7 +89,7 @@ resource "aws_nat_gateway" "main" {
 }
 
 resource "aws_route_table" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
 
   vpc_id = aws_vpc.main.id
 
@@ -101,12 +99,12 @@ resource "aws_route_table" "private" {
   }
 
   tags = {
-    Name = "private${count.index + 1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
